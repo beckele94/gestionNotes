700207020015ad52089ff40fa49773f85d746f7f
beckele94 <ulysseguillot@gmail.com>
2022-03-22 23:38:22 +0100

    instanciation attributs

--- src/gestionNotes.java
+++ src/gestionNotes.java
@@ -1,25 +1,29 @@
+import java.util.*;
 
 public class gestionNotes {
 
 	public static void main(String[] args) {
 		
 		//Affiche la liste des etudiants puis le prenom du premier
-		System.out.println(OutilsStringToObject.recupererLesEtudiants("files/etudiants.tsv"));
-		System.out.println(OutilsStringToObject.recupererLesEtudiants("files/etudiants.tsv").get(0).prenomEtudiant);
+		ArrayList<Etudiant> es = OutilsStringToObject.recupererLesEtudiants("files/etudiants.tsv");
+		System.out.println(es);
+		System.out.println(es.get(0).prenomEtudiant);
 		
 		//Affiche la liste des matieres puis le libelle de la premiere
-		System.out.println(OutilsStringToObject.recupererLesMatieres("files/matieres.tsv"));
-		System.out.println(OutilsStringToObject.recupererLesMatieres("files/matieres.tsv").get(0).libelleMatiere);
+		ArrayList<Matiere> ms = OutilsStringToObject.recupererLesMatieres("files/matieres.tsv");
+		System.out.println(ms);
+		System.out.println(ms.get(0).libelleMatiere);
 		
 		//Affiche la liste des devoirs puis le libelle de la matiere du premier devoir
-		System.out.println(OutilsStringToObject.recupererLesDevoirs("files/devoirs.tsv", OutilsStringToObject.recupererLesMatieres("files/matieres.tsv")));
-		System.out.println(OutilsStringToObject.recupererLesDevoirs("files/devoirs.tsv", OutilsStringToObject.recupererLesMatieres("files/matieres.tsv")).get(0).matiere.libelleMatiere);
+		ArrayList<Devoir> ds = OutilsStringToObject.recupererLesDevoirs("files/devoirs.tsv", ms);
+		System.out.println(ds);
+		System.out.println(ds.get(0).matiere.libelleMatiere);
 		
 		//Affiche la liste des notes puis le libelle de la matiere et le prenom de l'étudiant de la premiere note
-		System.out.println(OutilsStringToObject.recupererLesNotes("files/notes.tsv", OutilsStringToObject.recupererLesDevoirs("files/devoirs.tsv", OutilsStringToObject.recupererLesMatieres("files/matieres.tsv")), OutilsStringToObject.recupererLesEtudiants("files/etudiants.tsv")));
-		System.out.println(OutilsStringToObject.recupererLesNotes("files/notes.tsv", OutilsStringToObject.recupererLesDevoirs("files/devoirs.tsv", OutilsStringToObject.recupererLesMatieres("files/matieres.tsv")), OutilsStringToObject.recupererLesEtudiants("files/etudiants.tsv")).get(0).devoir.libelleDevoir);
-		System.out.println(OutilsStringToObject.recupererLesNotes("files/notes.tsv", OutilsStringToObject.recupererLesDevoirs("files/devoirs.tsv", OutilsStringToObject.recupererLesMatieres("files/matieres.tsv")), OutilsStringToObject.recupererLesEtudiants("files/etudiants.tsv")).get(0).etudiant.prenomEtudiant);
-
+		ArrayList<Note> ns = OutilsStringToObject.recupererLesNotes("files/notes.tsv", ds, es);
+		System.out.println(ns);
+		System.out.println(ns.get(0).devoir.libelleDevoir);
+		System.out.println(ns.get(0).etudiant.prenomEtudiant);
 	}
 }
 
